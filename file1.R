2b48bca42b6eb3097b87d7ffb389dacf8d12c16c
Jiemin <yangjiemin0105@gmail.com>
2023-03-23 10:54:38 -0400

    inital commit

---
+++ file1.R
@@ -0,0 +1,5 @@
+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
+
+credentials::set_github_pat()
+#> If prompted for GitHub credentials, enter your PAT in the password field
+#> Using GITHUB_PAT from Jennifer (Jenny) Bryan (credential helper: osxkeychain)
